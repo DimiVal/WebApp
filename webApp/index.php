6464dc0934b53d2416af75f04564c82a39c51595
DimiVal <dimivalkova@gmail.com>
2019-01-16 13:36:52 +0200

    webApp

---
+++ webApp/index.php
@@ -0,0 +1,18 @@
+
+
+<?php
+$myName = $_GET["myName"] . " Petrov";
+$myAge = $_GET["age"] + 50;
+
+
+?>
+
+<html>
+
+<p>
+
+My name is <?php echo $myName ?> and my age is <?php echo $myAge ?>
+
+</p>
+
+</html>
---
+++ webApp/product.php
@@ -0,0 +1,14 @@
+<?php
+$myName - "Dimi";
+$myAge - "5";
+?>
+
+<html>
+
+<p>
+
+My name is <?php echo $myName ?> and my age is <?php echo $myAge ?>
+
+</p>
+
+</html>
